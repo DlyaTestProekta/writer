9f6a922c704a9c79a8189582d9acc81c4d6d830d
Vadim <isako-vadim@yandex.ru>
2024-08-19 17:02:44 +0500

    REFORMAT

--- src/main/java/ru/pachan/writer/WriterApplication.java
+++ src/main/java/ru/pachan/writer/WriterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WriterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WriterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WriterApplication.class, args);
+    }
 
 }
